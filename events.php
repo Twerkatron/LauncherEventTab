7ddcda354f24c7fdf70152eef4a7956681d1ea8d
Benjamin Knecht <knecht.benjamin@yahoo.de>
2013-11-29 14:41:01 +0100

    Init

---
+++ events.php
@@ -0,0 +1,324 @@
+<!doctype html>
+<html>
+<head>
+	<title>Sleeves and Backgrounds</title>
+	
+	<link rel="stylesheet" href="css/tab-view.css" type="text/css" media="screen">
+	<script type="text/javascript" src="js/ajax.js"></script>
+	<script type="text/javascript" src="js/tab-view.js">
+	/************************************************************************************************************
+	(C) www.dhtmlgoodies.com, October 2005
+	
+	This is a script from www.dhtmlgoodies.com. You will find this and a lot of other scripts at our website.	
+	
+	Terms of use:
+	You are free to use this script as long as the copyright message is kept intact. However, you may not
+	redistribute, sell or repost it without our permission.
+	
+	Updated:
+		
+		March, 14th, 2006 - Create new tabs dynamically
+	
+	Thank you!
+	
+	www.dhtmlgoodies.com
+	Alf Magne Kalleland
+	
+	************************************************************************************************************/		
+	</script>
+</head>
+<body>
+<?php  include_once '../google/analytics.php'; ?>
+<div id="dhtmlgoodies_tabView1">
+	<div class="dhtmlgoodies_aTab">
+		
+          
+<div>
+    <h3>Weekly download specials</h3>
+    <p>Come back every sunday to discover brand new covers, background and decks! 
+        You can also upload your own custom stuff and share them with your friends.</p>
+        
+
+<p><b>Paket #1</b></p>        
+<img src="http://ygopro.de/img/cover/preview/cover2vorschau.jpg" alt="Cover 01" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover3vorschau.jpg" alt="Cover 02" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover4vorschau.jpg" alt="Cover 03" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover5vorschau.jpg" alt="Cover 04" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover6vorschau.jpg" alt="Cover 05" width="60" />
+<br>
+<a class="button" href="http://adf.ly/ZNHVB" target="blank">Download</a>
+    </div>
+
+<div class="boxSilver">    
+        <p><b>Paket #2</b></p>        
+<img src="http://ygopro.de/img/cover/preview/cover7vorschau.jpg" alt="Cover 07" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover8vorschau.jpg" alt="Cover 08" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover9vorschau.jpg" alt="Cover 09" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover10vorschau.jpg" alt="Cover 10" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover11vorschau.jpg" alt="Cover 11" width="60" />
+<br>
+<a class="button" href="http://adf.ly/ZNIZw" target="blank">Download</a>
+</div> 
+    
+<div class="boxSilver">    
+        <p><b>next Week Preview</b></p>        
+<img src="http://ygopro.de/img/cover/preview/cover12vorschau.jpg" alt="Cover 12" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover13vorschau.jpg" alt="Cover 13" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover14vorschau.jpg" alt="Cover 14" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover15vorschau.jpg" alt="Cover 15" width="60" />
+<img src="http://ygopro.de/img/cover/preview/cover16vorschau.jpg" alt="Cover 16" width="60" />
+
+
+    </div>
+    
+
+		
+		
+</div>
+    
+    
+<!-- Tab Themes -->
+<div class="dhtmlgoodies_aTab">
+    <p>The Themes here are created by Users.</p>
+            
+    <h5>DevPro Theme 1.0 by ALu</h5>
+    <a class="button" href="http://adf.ly/ZqZrz" target="blank">Download</a>
+
+    
+    <h5>Egypt Theme by Aizan1412</h5>
+    <img src="http://i946.photobucket.com/albums/ad302/Aizan1412/3_zpscaa27aa4.jpg" alt="Egypt Theme Preview 1" width="150" />
+    <img src="http://i946.photobucket.com/albums/ad302/Aizan1412/5_zps3c3d7c03.jpg" alt="Egypt Theme Preview 2" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/ZqcEK" target="blank">Download</a>
+    
+    <h5>Archfiend Empress Theme by -xXPAINXx-</h5>
+    <img src="http://i1251.photobucket.com/albums/hh559/TheSleepwalker17/empress_zps2a0b6ed1.jpg" alt="Egypt Theme Preview 1" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/Zqdl9" target="blank">Download</a>
+    
+    <h5>Blazblue Theme by -xXPAINXx-</h5>
+    <img src="http://i1251.photobucket.com/albums/hh559/TheSleepwalker17/NoelVermillionThemeScreenshot_zpsdccbcb25.png" alt="Egypt Theme Preview 1" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/ZqenM" target="blank">Download</a>
+    
+    <h5>Chaos Goddess Theme by -xXPAINXx-</h5>
+    <img src="http://i1251.photobucket.com/albums/hh559/TheSleepwalker17/GoddessScreen_zps7a35b611.jpg" alt="Chaos Goddess Theme Preview 1" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/Zrnjw" target="blank">Download</a>
+    
+    <h5>Darkroom Skin Theme by Krainz</h5>
+    <img src="http://i.imgur.com/srrvYNZ.jpg" alt="Chaos Goddess Theme Preview 1" width="150" />
+    <img src="http://i.imgur.com/kV1AXWm.jpg" alt="Chaos Goddess Theme Preview 2" width="150" />
+    <img src="http://i.imgur.com/JFRg6i3.jpg" alt="Chaos Goddess Theme Preview 3" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/ZrohV" target="blank">Download</a>
+    
+    <h5>White T Theme by JulioCtC</h5>
+    <img src="http://img600.imageshack.us/img600/4586/jctc201301.jpg" alt="White T Theme Preview 1" width="150" />
+    <img src="http://img255.imageshack.us/img255/569/jctc201304.jpg" alt="White T Theme Preview 2" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/ZrpZj" target="blank">Download</a>
+    
+    <h5>Yugioh 3 Online Theme by Jazztc</h5>
+    <img src="http://imageshack.us/a/img853/5922/29810263.jpg" alt="Yugioh Online 3 Theme Preview 1" width="150" />
+    <img src="http://imageshack.us/a/img203/8707/21240740.jpg" alt="Yugioh Onlline 3 Theme Preview 2" width="150" />
+    <img src="http://i.imgur.com/W0o482V.jpg" alt="Yugioh Onlline 3 Theme Preview 2" width="150" />
+    <br>
+    <a class="button" href="http://adf.ly/ZrrSG" target="blank">Download</a>
+</div>
+
+<!-- Tab Backgrounds  -->    
+<div class="dhtmlgoodies_aTab">
+    <h3>Backgrounds</h3>
+    <p>Click on the image to download the background.</p>
+    <a href="http://adf.ly/ZqsVC" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/bgb6sy2glr89_thumb.jpg" border="0"></a>
+    <a href="http://adf.ly/ZqsoX" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/bgt827vcq6j0_thumb.jpg" border="0"></a>
+    <a href="http://adf.ly/ZrqFL" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/bg6ekrvu7hnp_thumb.jpg" border="0"></a>
+    <a href="http://adf.ly/a1sZU" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/devbg1hz9aypj36q_thumb.jpg" border="0"></a>
+    
+    <a href="http://adf.ly/a1sjj" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/devbg2bluejegoi3awdt_thumb.jpg" border="0"></a>
+    <a href="http://adf.ly/a1swE" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/devbg2greengit0kd314v_thumb.jpg" border="0"></a>
+    <a href="http://adf.ly/a1tFf" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/devbg2orangeoka4ibhnz6_thumb.jpg" border="0"></a>
+    <a href="http://adf.ly/a1tPw" target="blank"><img src="http://img5.fotos-hochladen.net/thumbnail/devbg2violetmn6l3scbhk_thumb.jpg" border="0"></a>
+</div>  
+
+<!-- Tab Decks  -->    
+<div class="dhtmlgoodies_aTab">
+    <h3>Decks</h3>
+    <p>These Decks are DevPro-"Structure-Decks". The aren't completely finished yet, 
+        so you have to work on them to get them more competetive. 
+        Each Deck includes 40 Main Deck Cards, 10 Extra Deck Cards and 10 Side Deck Cards.</p>
+    
+    <div id="col1">
+    <h5 class="center">Archfiends</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Archfiends.jpg" alt="Archfiends" width="90" >
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mPB" target="blank">Download</a>
+    </div>
+    
+    <div id="col2">
+    <h5 class="center">Blackwings</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Blackwing.jpg" alt="Blackwings" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mTj" target="blank">Download</a>
+    </div>
+    
+    <div id="col3">
+    <h5 class="center">Bujin</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Bujin.jpg" alt="Bujin" width="90" >
+    <ul>
+        <li>Format: TCG/OCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mYP" target="blank">Download</a>
+    </div>
+    
+    <div id="col4">
+    <h5 class="center">Chaos Dragon</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_ChaosDragon.jpg" alt="Chaos Dragon" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mb9" target="blank">Download</a>
+    </div>
+    
+    <!-- 2. Zeile  --> 
+    
+    <div id="col1">
+    <h5 class="center">Crystal Beast</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_CrystalBeast.jpg" alt="Crystal Beast" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mev" target="blank">Download</a>
+    </div>
+    
+    <div id="col2">
+    <h5 class="center">Galaxy Photon</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_GalaxyPhoton.jpg" alt="Galaxy Photon" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mky" target="blank">Download</a>
+    </div>
+    
+    <div id="col3">
+    <h5 class="center">Geargia</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Geargia.jpg" alt="Geargia" width="90" />
+    <ul>
+        <li>Format: TCG/OCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mpY" target="blank">Download</a>
+    </div>
+    
+    <div id="col4">
+    <h5 class="center">Ghostrick</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Ghostrick.jpg" alt="Ghostrick" width="90" />
+    <ul>
+        <li>Format: TCG/OCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mt2" target="blank">Download</a>
+    </div>
+    
+    <!-- 3. Zeile  -->
+    
+    <div id="col1">
+    <h5 class="center">Gravekeeper</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Gravekeeper.jpg" alt="Gravekeeper" width="90" />
+    <ul>
+        <li>Format: TCG/OCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1mwt" target="blank">Download</a>
+    </div>
+    
+    <div id="col2">
+    <h5 class="center">Harpies</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Harpies.jpg" alt="Harpies" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1n14" target="blank">Download</a>
+    </div>
+    
+    <div id="col3">
+    <h5 class="center">Madolche</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_Madolche.jpg" alt="Madolche" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1n4U" target="blank">Download</a>
+    </div>
+    
+    <div id="col4">
+    <h5 class="center">Mecha Phantom</h5>
+    <img class="center" src="images/decks/Decktemplates/DeckBox_MechaPhantom.jpg" alt="Mecha Phantom" width="90" />
+    <ul>
+        <li>Format: TCG</li>
+        <li>Banlist: 09.2013</li>
+    </ul>
+    <a class="deckbutton" href="http://adf.ly/a1n7z" target="blank">Download</a>
+    </div>
+</div>
+
+    
+<!-- Tab User Uploads  -->    
+<div class="dhtmlgoodies_aTab">
+    <p>Users can upload covers, backgrounds and themes. We share some of your 
+    uploads with the community. This feature will coming soon.</p>
+    <p><a href="http://ygopro.de/login.php?sites=login">DevPro Web Login</a> for DevPro uploads. 
+    Currently you can only upload decks.</p>
+</div>
+
+<!-- Tab FAQ  -->    
+<div class="dhtmlgoodies_aTab">
+
+    <h5>How to use a Custom card sleeve?</h5>
+        <p>
+        In your game folder "textures" you can replace "cover.jpg" and "cover2.jpg" 
+        with your own sleeves. You only see this cardsleeves in your duels. If you want 
+        that your oppenent see your custom sleeve you can upload the sleeve to our server. 
+        <a href="http://ygopro.de/login.php?sites=login">ygopro.de login</a>
+        </p>
+    <h5>How to use a Custom Background?</h5>
+        <p>
+   In your game folder "textures" you can replace "bg.jpg" and "bg2.jpg" 
+         with your own backgrounds. You only see this backgrounds.
+  </p>
+    <h5>How to use a Custom Theme?</h5>
+  <p>
+  In your game folder "skins" you can place the folder with the skin and in the game folder "textures" you can replace the graphics.
+  Then you must select the skin in the options and enable directx. You only see this theme.
+  </p>
+ <h5>How to use a deck?</h5> 
+  <p>
+  In your game folder "deck" you can place the decks.
+  </p>
+ <h5>How do download these things?</h5> 
+  <p>
+  Click on the download button. This opens a page.
+  You must wait on the page before you can skip the advertisement.
+  Then you can download the stuff.
+  </p>
+
+</div>    
+        
+</div>
+<script type="text/javascript">
+initTabs('dhtmlgoodies_tabView1',Array('Sleeves','Themes','Backgrounds','Decks','User Uploads','FAQ'),0,700,1600);
+</script>
+</body>
+</html>
